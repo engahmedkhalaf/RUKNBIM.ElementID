5dc9fff279071bff24092071377530999038a6b4
Ahmed Khalaf <you@example.com>
2026-06-24 09:02:19 +0300

    Update API name to Rukn, add Info command with live update checker, and configure user-defined ribbon icons

--- AddinRibbon.cs
+++ AddinRibbon.cs
@@ -1,5 +1,8 @@
 using Autodesk.Navisworks.Api.Plugins;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using Autodesk.Navisworks.Api;
 
 namespace RUKNBIM.ElementID
@@ -17,48 +20,25 @@ namespace RUKNBIM.ElementID
         }
     }
 
-    [PluginAttribute("RUKNBIM.ElementID", "RUKN", DisplayName = "KH Addins")]
+    [PluginAttribute("RUKNBIM.ElementID", "RUKN", DisplayName = "Rukn")]
     [Strings("RUKNBIM.ElementID.name")]
     [RibbonLayout("RUKNBIM.ElementID.xaml")]
-    [RibbonTab("KHAddinsTab", DisplayName = "KH Addins")]
-    [Command("SelectByRevitID", DisplayName = "Select By\nRevit ID", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("ZoomToSelection", DisplayName = "Zoom To\nSelection", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("IsolateSelection", DisplayName = "Isolate\nSelection", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("ClearIsolation", DisplayName = "Clear\nIsolation", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("AutoSectionBox", DisplayName = "Auto\nSection Box", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("ClearSectionBox", DisplayName = "Clear\nSection Box", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
-    [Command("SaveViewpoint", DisplayName = "Save\nViewpoint", Icon = "Images\\ElementID_16.ico", LargeIcon = "Images\\ElementID_32.png")]
+    [RibbonTab("RuknTab", DisplayName = "Rukn")]
+    [Command("ZoomToSelection", DisplayName = "Zoom To\nSelection", Icon = "Images\\R_icon_blue_16px.png", LargeIcon = "Images\\R_icon_blue_32px.png")]
+    [Command("IsolateSelection", DisplayName = "Isolate\nSelection", Icon = "Images\\R_icon_green_16px.png", LargeIcon = "Images\\R_icon_green_32px.png")]
+    [Command("ClearIsolation", DisplayName = "Clear\nIsolation", Icon = "Images\\R_icon_teal_16px.png", LargeIcon = "Images\\R_icon_teal_32px.png")]
+    [Command("AutoSectionBox", DisplayName = "Auto\nSection Box", Icon = "Images\\R_icon_orange_16px.png", LargeIcon = "Images\\R_icon_orange_32px.png")]
+    [Command("ClearSectionBox", DisplayName = "Clear\nSection Box", Icon = "Images\\R_icon_red_16px.png", LargeIcon = "Images\\R_icon_red_32px.png")]
+    [Command("SaveViewpoint", DisplayName = "Save\nViewpoint", Icon = "Images\\R_icon_gold_16px.png", LargeIcon = "Images\\R_icon_gold_32px.png")]
+    [Command("ShowInfo", DisplayName = "Info", Icon = "Images\\Info_16.png", LargeIcon = "Images\\Info_32.png")]
     public class RibbonCommandHandler : CommandHandlerPlugin
     {
-        private static Window _navigatorWindow;
-
         public override int ExecuteCommand(string name, params string[] parameters)
         {
             var doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
 
             switch (name)
             {
-                case "SelectByRevitID":
-                    if (_navigatorWindow == null)
-                    {
-                        var view = new ElementIDView();
-                        _navigatorWindow = new Window
-                        {
-                            Title = "Element ID",
-                            Content = view,
-                            Width = 350,
-                            Height = 550,
-                            Topmost = true,
-                            WindowStartupLocation = WindowStartupLocation.CenterScreen
-                        };
-                        _navigatorWindow.Closed += (s, e) => _navigatorWindow = null;
-                        _navigatorWindow.Show();
-                    }
-                    else
-                    {
-                        _navigatorWindow.Focus();
-                    }
-                    break;
                 case "ZoomToSelection":
                     NavisworksActions.FocusCamera(doc);
                     break;
@@ -80,8 +60,164 @@ namespace RUKNBIM.ElementID
                 case "SaveViewpoint":
                     NavisworksActions.SaveViewpoint(doc, "");
                     break;
+                case "ShowInfo":
+                    ShowInfoWindow();
+                    break;
             }
             return 0;
+        }
+
+        private static void ShowInfoWindow()
+        {
+            var localAssemblyVer = typeof(RibbonCommandHandler).Assembly.GetName().Version;
+            var localVer = new System.Version(localAssemblyVer.Major, localAssemblyVer.Minor, localAssemblyVer.Build);
+
+            var window = new Window
+            {
+                Title = "RUKNBIM Info",
+                Width = 420,
+                Height = 250,
+                Background = new SolidColorBrush((System.Windows.Media.Color)ColorConverter.ConvertFromString("#0B132B")),
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                ResizeMode = ResizeMode.NoResize,
+                Topmost = true,
+                WindowStyle = WindowStyle.ToolWindow
+            };
+
+            var stackPanel = new StackPanel { Margin = new Thickness(25), VerticalAlignment = VerticalAlignment.Center };
+
+            var versionLabel = new TextBlock
+            {
+                Text = $"Version: {localAssemblyVer}",
+                Foreground = Brushes.White,
+                FontSize = 14,
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(0, 0, 0, 4)
+            };
+            stackPanel.Children.Add(versionLabel);
+
+            var updateStatusLabel = new TextBlock
+            {
+                Text = "Checking for updates...",
+                Foreground = Brushes.Gray,
+                FontSize = 11,
+                Margin = new Thickness(0, 0, 0, 12)
+            };
+            stackPanel.Children.Add(updateStatusLabel);
+
+            var preparedLabel = new TextBlock
+            {
+                Text = "Prepared by: Ahmed Khalaf (BIM Manager)",
+                Foreground = Brushes.LightGray,
+                FontSize = 13,
+                Margin = new Thickness(0, 0, 0, 8)
+            };
+            stackPanel.Children.Add(preparedLabel);
+
+            var mobileLabel = new TextBlock
+            {
+                Text = "Mobile: +966542554127",
+                Foreground = Brushes.LightGray,
+                FontSize = 13,
+                Margin = new Thickness(0, 0, 0, 15)
+            };
+            stackPanel.Children.Add(mobileLabel);
+
+            // Contact Support
+            var supportPanel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 0, 0, 8) };
+            var supportLabel = new TextBlock
+            {
+                Text = "Contact Support: ",
+                Foreground = Brushes.LightGray,
+                FontSize = 13
+            };
+            supportPanel.Children.Add(supportLabel);
+
+            var supportLink = new TextBlock
+            {
+                Text = "engkhalaf7@gmail.com",
+                Foreground = new SolidColorBrush((System.Windows.Media.Color)ColorConverter.ConvertFromString("#3A86FF")),
+                FontSize = 13,
+                Cursor = Cursors.Hand,
+                TextDecorations = TextDecorations.Underline
+            };
+            supportLink.MouseDown += (s, e) => {
+                try { System.Diagnostics.Process.Start("mailto:engkhalaf7@gmail.com"); } catch { }
+            };
+            supportPanel.Children.Add(supportLink);
+            stackPanel.Children.Add(supportPanel);
+
+            // Website
+            var webPanel = new StackPanel { Orientation = Orientation.Horizontal };
+            var webLabel = new TextBlock
+            {
+                Text = "Website: ",
+                Foreground = Brushes.LightGray,
+                FontSize = 13
+            };
+            webPanel.Children.Add(webLabel);
+
+            var webLink = new TextBlock
+            {
+                Text = "rukn-bim-website-opka.vercel.app",
+                Foreground = new SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#3A86FF")),
+                FontSize = 13,
+                Cursor = Cursors.Hand,
+                TextDecorations = TextDecorations.Underline
+            };
+            webLink.MouseDown += (s, e) => {
+                try { System.Diagnostics.Process.Start("https://rukn-bim-website-opka.vercel.app"); } catch { }
+            };
+            webPanel.Children.Add(webLink);
+            stackPanel.Children.Add(webPanel);
+
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                try
+                {
+                    using (var client = new System.Net.WebClient())
+                    {
+                        client.Headers.Add("User-Agent", "Rukn-Bim-Plugin");
+                        string xml = client.DownloadString("https://raw.githubusercontent.com/engahmedkhalaf/RUKNBIM.ElementID/main/PackageContents.xml");
+                        var match = System.Text.RegularExpressions.Regex.Match(xml, "AppVersion=\"([^\"]+)\"");
+                        if (match.Success)
+                        {
+                            var remoteVer = new System.Version(match.Groups[1].Value);
+
+                            window.Dispatcher.Invoke(() =>
+                            {
+                                if (remoteVer > localVer)
+                                {
+                                    updateStatusLabel.Text = $"Update available: v{remoteVer} (Click to open GitHub)";
+                                    updateStatusLabel.Foreground = new SolidColorBrush((System.Windows.Media.Color)ColorConverter.ConvertFromString("#FFD000"));
+                                    updateStatusLabel.Cursor = Cursors.Hand;
+                                    updateStatusLabel.TextDecorations = TextDecorations.Underline;
+                                    updateStatusLabel.MouseDown += (s, e) =>
+                                    {
+                                        try { System.Diagnostics.Process.Start("https://github.com/engahmedkhalaf/RUKNBIM.ElementID"); } catch { }
+                                    };
+                                }
+                                else
+                                {
+                                    updateStatusLabel.Text = "Plugin is up to date";
+                                    updateStatusLabel.Foreground = Brushes.LightGreen;
+                                }
+                            });
+                        }
+                    }
+                }
+                catch
+                {
+                    window.Dispatcher.Invoke(() =>
+                    {
+                        updateStatusLabel.Text = "Could not check for updates";
+                        updateStatusLabel.Foreground = Brushes.Gray;
+                    });
+                }
+            });
+
+            window.Content = stackPanel;
+            window.ShowDialog();
         }
     }
 }
